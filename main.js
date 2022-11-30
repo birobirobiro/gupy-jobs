4f2b310d838c086737d52561d0ebfe698b4095f0
João Inácio Neto <birobirobiro.dev@gmail.com>
2022-11-30 02:15:55 -0300

    chore: ajustes visuais

--- main.js
+++ main.js
@@ -5,10 +5,10 @@ const jobState = document.getElementById('job-state');
 const jobDate = document.getElementById('job-date');
 const jobLink = document.getElementById('job-link');
 const jobLogo = document.getElementById('job-logo');
+const jobRemote = document.getElementById('job-remote');
 
 const fetchGupy = async (gupy) => {
   const APIResponse = await fetch(`https://cors-everywhere.onrender.com/https://portal.gupy.io/_next/data/lSTZemWfxCuGRPIikwn3c/pt/job-search/term=Analista%20financeiro.json`);
-
 
   if (APIResponse.status === 200) {
     const data = await APIResponse.json();
@@ -23,19 +23,26 @@ const renderGupy = async (gupy) => {
 
   const data = await fetchGupy(gupy)
 
-  jobCompany.innerHTML = data['pageProps']['dehydratedState']['queries'][38]['state']['data']['pages'][0]['data'][0]['careerPageName']
+  jobCompany.innerHTML = data['pageProps']['dehydratedState']['queries'][0]['state']['data']['pages'][0]['data'][0]['careerPageName']
 
-  jobTitle.innerHTML = data['pageProps']['dehydratedState']['queries'][38]['state']['data']['pages'][0]['data'][0]['name']
+  jobTitle.innerHTML = data['pageProps']['dehydratedState']['queries'][0]['state']['data']['pages'][0]['data'][0]['name']
 
-  jobCity.innerHTML = data['pageProps']['dehydratedState']['queries'][38]['state']['data']['pages'][0]['data'][0]['city']
+  jobCity.innerHTML = data['pageProps']['dehydratedState']['queries'][0]['state']['data']['pages'][0]['data'][0]['city']
 
-  jobState.innerHTML = data['pageProps']['dehydratedState']['queries'][38]['state']['data']['pages'][0]['data'][0]['state']
+  jobState.innerHTML = data['pageProps']['dehydratedState']['queries'][0]['state']['data']['pages'][0]['data'][0]['state']
 
-  jobDate.innerHTML = data['pageProps']['dehydratedState']['queries'][38]['state']['data']['pages'][0]['data'][0]['publishedDate']
+  jobDate.innerHTML = `Data da vaga: ` + data['pageProps']['dehydratedState']['queries'][0]['state']['data']['pages'][0]['data'][0]['publishedDate']
 
-  jobLink.innerHTML = data['pageProps']['dehydratedState']['queries'][38]['state']['data']['pages'][0]['data'][0]['careerPageUrl']
+  jobLink.innerHTML = data['pageProps']['dehydratedState']['queries'][0]['state']['data']['pages'][0]['data'][0]['careerPageUrl']
 
-  jobLogo.src = data['pageProps']['dehydratedState']['queries'][38]['state']['data']['pages'][0]['data'][0]['careerPageLogo']
+  jobLogo.src = data['pageProps']['dehydratedState']['queries'][0]['state']['data']['pages'][0]['data'][0]['careerPageLogo']
+
+  if (jobLogo.src === undefined || null) {
+    jobLogo.src = 'https://via.placeholder.com/150'
+  }
+
+  jobRemote.innerHTML = data['pageProps']['dehydratedState']['queries'][0]['state']['data']['pages'][0]['data'][0]['isRemoteWork'] === true ? 'Remoto: Sim' : 'Remoto: Não'
+
 }
 
 renderGupy()
