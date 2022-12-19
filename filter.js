a00c7a438fd78f43bdd47deda7e5d6422b535d8d
João Inácio Neto <birobirobiro.dev@gmail.com>
2022-12-19 16:04:41 -0300

    fix: filter

--- filter.js
+++ filter.js
@@ -1,12 +1,16 @@
 // filter hide/show
-var htmlShow = document.getElementById("filter-show");
+const htmlShow = document.getElementById("filter-show");
 htmlShow.style.display = "none";
 
+const filterButton = document.getElementById("filter");
+
 export function ShowFilter() {
-  var htmlShow = document.getElementById("filter-show");
+  const htmlShow = document.getElementById("filter-show");
   if (htmlShow.style.display === "none") {
     htmlShow.style.display = "block";
   } else {
     htmlShow.style.display = "none";
   }
 }
+
+filterButton.addEventListener("click", ShowFilter)
